62f2d434427148a1b6aec42ec1bdd7ccd53f2f6a
Sabit Zahin <sgtlaugh@gmail.com>
2021-05-31 18:45:02 +0600

    Improve test runner script

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+script_start=`date +%s%N`
+
 python extract_snippets.py
 
 if [ -z "$CXX" ];
@@ -6,34 +9,49 @@ then
     CXX=g++
 fi
 
-TESTS=0
-SUCCESS=0
+red=`tput setaf 1`
+green=`tput setaf 2`
+no_color=`tput sgr0`
+
+total=0
+success=0
+
 for cppfile in *.cpp;
 do
-    TESTS=$((TESTS + 1))
+    echo -n "Running $cppfile - "
+
+    total=$((total + 1))
+    start=`date +%s%N`
+
     $CXX -std=c++11 $cppfile -o $cppfile.out 
     COMPILATION=$?
     if [ $COMPILATION -eq 0 ];
     then
         ./$cppfile.out
-        TEST_SUCCESS=$?
-        if [ $TEST_SUCCESS -eq 0 ];
+        test_success=$?
+        if [ $test_success -eq 0 ];
         then
-            SUCCESS=$((SUCCESS + 1))
+            success=$((success + 1))
+            end=`date +%s%N`
+            echo ${green}Passed in $(((end - $start)/1000000)) ms${no_color}
         else
-            echo "Test $cppfile failed!"
+            echo ${red}"Test $cppfile failed!"${no_color}
         fi
-    else
-        echo "Error while compiling $cppfile!"
-    fi
 
-    rm $cppfile.out
+        rm $cppfile.out
+    else
+        echo ${red}"Error while compiling $cppfile!"${no_color}
+    fi
 done
 
-echo "$SUCCESS / $TESTS tests were successful."
-if [ $SUCCESS -eq $TESTS ];
+script_end=`date +%s%N`
+time_taken=$(echo "$script_start $script_end" | awk '{printf "%.2f\n", ($2-$1)/1000000000.0}')
+
+if [ $success -eq $total ];
 then
+    echo ${green}"\n$success PASSED in $time_taken seconds${no_color}"
     exit 0
 else
+    echo ${red}"\n$(($total - $success)) FAILED, ${green}$success PASSED${red} in $time_taken seconds${no_color}"
     exit 1
 fi
