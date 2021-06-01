a2bcb2ff224c2672f6b0d57589a2bcccc18b08fe
Sabit Zahin <sgtlaugh@gmail.com>
2021-06-02 04:58:47 +0600

    Improve test runner script

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 script_start=`date +%s%N`
 
@@ -13,14 +13,12 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 no_color=`tput sgr0`
 
-total=0
 success=0
+failed_tests=()
 
 for cppfile in *.cpp;
 do
     echo -n "Running $cppfile - "
-
-    total=$((total + 1))
     start=`date +%s%N`
 
     $CXX -std=c++17 -fsanitize=undefined -fno-sanitize-recover $cppfile -o $cppfile.out 
@@ -35,23 +33,29 @@ do
             end=`date +%s%N`
             echo ${green}Passed in $(((end - $start)/1000000)) ms${no_color}
         else
-            echo ${red}"Test $cppfile failed!"${no_color}
+            echo ${red}"$cppfile failed!"${no_color}
+            failed_tests+=($cppfile)
         fi
 
         rm $cppfile.out
     else
-        echo ${red}"Error while compiling $cppfile!"${no_color}
+        echo ${red}"Compilation error on $cppfile!"${no_color}
+        failed_tests+=($cppfile)
     fi
 done
 
 script_end=`date +%s%N`
 time_taken=$(echo "$script_start $script_end" | awk '{printf "%.2f\n", ($2-$1)/1000000000.0}')
 
-if [ $success -eq $total ];
+if [ ${#failed_tests[@]} -eq 0 ];
 then
-    echo ${green}"\n$success PASSED in $time_taken seconds${no_color}"
+    echo -e ${green}"\n$success PASSED in $time_taken seconds${no_color}"
     exit 0
 else
-    echo ${red}"\n$(($total - $success)) FAILED, ${green}$success PASSED${red} in $time_taken seconds${no_color}"
+    echo -e ${red}"\n${#failed_tests[@]} FAILED, ${green}$success PASSED${red} in $time_taken seconds${no_color}\n"
+    for cppfile in "${failed_tests[@]}"
+    do
+        echo ${red}"$cppfile failed!"${no_color}
+    done
     exit 1
 fi
