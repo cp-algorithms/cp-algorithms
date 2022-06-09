eab5eea214caf9b7d721c4eae8a061b01c8b5a7b
Oleksandr Kulkov <melfice@google.com>
2022-06-09 02:53:14 +0200

    make install script

---
+++ scripts/install-mkdocs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip install \
+    mkdocs-material \
+    mkdocs-macros-plugin \
+    mkdocs-literate-nav \
+    mkdocs-git-authors-plugin \
+    mkdocs-git-revision-date-localized-plugin \
+    mkdocs-simple-hooks \
+    plugins/mkdocs-git-committers-plugin-2
