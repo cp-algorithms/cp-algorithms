2638b5b3dd259f65befde13a3fa3c93b554b4429
Oleksandr Kulkov <melfice@google.com>
2023-06-26 17:45:29 +0200

    Update install-mkdocs.sh
    install mkdocs plugin

--- scripts/install-mkdocs.sh
+++ scripts/install-mkdocs.sh
@@ -7,4 +7,5 @@ pip install \
     mkdocs-git-authors-plugin \
     mkdocs-git-revision-date-localized-plugin \
     mkdocs-simple-hooks \
+    mkdocs-rss-plugin \
     plugins/mkdocs-git-committers-plugin-2
