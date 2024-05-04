cbcce391e0f2b08aed5598b4b93ebfd5b6b94b58
NaimSS <naimsantos2002@gmail.com>
2024-05-04 11:20:22 +0200

    improve test cases

--- test/test_bridge_searching_offline.cpp
+++ test/test_bridge_searching_offline.cpp
@@ -25,24 +25,60 @@ void test_suite(
     n = _n;
     adj = _adj;
     find_bridges();
-    normalize(&expected_bridges);
-    normalize(&bridges);
+    normalize(expected_bridges);
+    normalize(bridges);
     assert(bridges == expected_bridges);
 }
 
 int main() {
+    vector<vector<int>> adj;
+    auto add_edge = [&](int a,int b){
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    };
     {
         int n = 5;
-        vector<vector<int>> adj(n);
-        adj[0].push_back(1), adj[1].push_back(0);
-        adj[0].push_back(1), adj[1].push_back(0);
+        adj.resize(n);
+        add_edge(0, 1);
+        add_edge(0, 1);
         
-        adj[2].push_back(3), adj[3].push_back(2);
-        adj[3].push_back(4), adj[4].push_back(3);
+        add_edge(2, 3);
+        add_edge(3, 4);
         vector<pair<int,int> > expected_bridges;
         expected_bridges.push_back({2,3});
         expected_bridges.push_back({3,4});
         // note that 0-1 is not a bridge due to duplicate edges!
         test_suite(n, adj, expected_bridges);
+        adj.clear();
+    }
+    {
+        int n = 7;
+        adj.resize(n);
+        add_edge(0, 1);
+        add_edge(1, 2);
+        add_edge(2, 3);
+        add_edge(3, 1);
+        add_edge(3, 4);
+        add_edge(0, 4);
+        add_edge(4, 5);
+        add_edge(1, 6);
+        vector<pair<int,int> > expected_bridges;
+        expected_bridges.push_back({4, 5});
+        expected_bridges.push_back({1, 6});
+        test_suite(n, adj, expected_bridges);
+        adj.clear();   
+    }
+    {
+        int n = 4;
+        adj.resize(n);
+        add_edge(0, 1);
+        add_edge(0, 1);
+        add_edge(1, 2);
+        add_edge(2, 3);
+        add_edge(2, 3);
+        vector<pair<int,int> > expected_bridges;
+        expected_bridges.push_back({1, 2});
+        test_suite(n, adj, expected_bridges);
+        adj.clear();   
     }
 }
