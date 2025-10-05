cecba3a469a5a7256845a42ebd2e8cdc79ca7a27
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2025-10-05 22:22:42 +0530

    PR review comments

--- scripts/install-mkdocs.sh
+++ scripts/install-mkdocs.sh
@@ -9,4 +9,4 @@ pip install \
     mkdocs-git-revision-date-localized-plugin \
     mkdocs-simple-hooks \
     mkdocs-rss-plugin \
-    mkdocs-git-committers-plugin-2
+    plugins/mkdocs-git-committers-plugin-2
